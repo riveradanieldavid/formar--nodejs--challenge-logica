034fff1508549126c82b91456791b84feefca77c
riveradanieldavid <dannieldavvid2004@gmail.com>
2023-05-16 21:39:25 -0300

    rda > 2 ejercicios hechos

--- number_cardinality.js
+++ number_cardinality.js
@@ -1,1 +1,66 @@
-asdfas
+// Escriba la función number_cardinality() que toma un número entero y devuelve una cadena, de la siguiente manera:
+//     Devuelve la cadena zero si el número termina en 0
+//     Devuelve la cadena five si el número termina en 5
+//     Devuelve la cadena even si el número es par (divisible por 2) Y no termina en 0
+//     Devuelve la cadena odd si el número es impar (no divisible por 2) Y no termina en 5
+// Asegúrese de que su cadena de retorno sea exactamente como la anterior, usando letras minúsculas.
+
+// Ejemplos:
+// Input: 100
+// Output: zero
+// Input: 88
+// Output: even
+// Input: 155
+// Output: five
+// Input: 99
+// Output: odd
+
+function number_cardinality2(NUMBER) {
+    if (NUMBER.toString().endsWith("0")) {
+        return "zero";
+    } else if (NUMBER.toString().endsWith("5")) {
+        return "five";
+    } else if (NUMBER % 2 === 0) { //CUANDO NUMBER DIVIDIDO 2 SU RESIDUO SEA 0 SIGNIFICA QUE ES PAR // NINGUN NUMERO TERMINADO EN 5 ES PAR.
+        return "even";
+    } else {
+        return "odd";
+    }
+}
+
+console.log(number_cardinality2(110));
+console.log(number_cardinality2(12));
+console.log(number_cardinality2(155));
+console.log(number_cardinality2(92));
+console.log(number_cardinality2(221));
+console.log(number_cardinality2(54353425));
+console.log(number_cardinality2(12546005));
+console.log(number_cardinality2(903453002));
+
+
+
+
+
+
+
+// OTRA FORMA
+// function number_cardinality(number) {
+//     if (number % 10 === 0) {
+//         return "zero";
+//     } else if (number % 10 === 5) {
+//         return "five";
+//     } else if (number % 2 === 0) {
+//         return "even";
+//     } else {
+//         return "odd";
+//     }
+// }
+
+// TEST1
+// let cinco = 52305;
+// let escinco = cinco.toString().endsWith("5")
+// console.log(escinco); //TRUE
+
+
+
+
+
--- range_vision.js
+++ range_vision.js
@@ -1,1 +1,55 @@
-dsasdfas
+// Crear la función range_vision() que, dado un entero no negativo, determine si hay al menos un dígito 1 en el entero que tiene el valor de visualización más bajo de todos los dígitos del entero. La función debe devolver un booleano: true cuando un dígito 1 tiene el valor de vista más bajo en el número dado, false de lo contrario. 
+
+// Imagine un número entero no negativo, digamos 34315. Cada dígito en el número entero tiene un llamado rango de visión . El primer dígito, 3, tiene un rango de visión de 3. En otras palabras, puede 3 "ver" 3 dígitos a cada lado de sí mismo. En nuestro ejemplo, puede mirar a la derecha de sí mismo y ver 4, 3 y 1. A la izquierda, no hay nada. El rango de visión de cada dígito es su propio valor.
+
+// Ahora bien, el rango de visión de un dígito define el llamado valor de visión, que es la suma de los dígitos dentro del rango de visión. Entonces, el valor de visión del primer dígito, 3 es 4+3+1=8. Para el segundo dígito, 4 el valor de visión es 3+3+1+5=12.
+
+// Ejemplos:
+
+// Para el número 34315:
+
+// Dígito: 3
+// Puede ver a la izquierda: -
+// Puede ver a la derecha: 431
+// Valor de la visión:4 + 3 + 1 = 8
+
+// Dígito: 4
+// Se puede ver a la izquierda: 3
+// Se puede ver a la derecha: 315
+// Valor de la visión:3 + 3 + 1 + 5 = 12
+
+// Dígito: 3
+// Puede ver a la izquierda: 34
+// Puede ver a la derecha: 15
+// Valor de la visión:3 + 4 + 1 + 5 = 13
+
+// Dígito: 1
+// Se puede ver a la izquierda: 3
+// Se puede ver a la derecha: 5
+// Valor de la visión:3 + 5 = 8
+
+// Dígito: 5
+// Puede ver a la izquierda: 3431
+// Puede ver a la derecha: -
+// valor de la visión:3 + 4 + 3 + 1 = 11
+
+// ¿Hay un dígito 1 en el número entero para el cual el valor de visión es mínimo?
+
+// Sí, 8 es el valor mínimo de visión, tanto para el primer 3 como para el 1. Eso satisface los criterios, por lo que la función devuelve true.
+
+// 22919 false
+// 34315 true
+function range_vision(N) {
+
+
+}
+
+
+
+
+
+
+
+
+
+
